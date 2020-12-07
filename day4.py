907bf2f13330d98096487e1ecf6145fd7dabfc70
Pedro Madrid <pmadrid@gmail.com>
2020-12-07 10:54:18 -0400

    day 4 fix

--- day4.py
+++ day4.py
@@ -2,14 +2,14 @@ import re
 with open("./input4.txt", encoding="utf-8") as file:
     passports = [l.rstrip("\n") for l in file]
 
-fields_regex = {'byr':'19[2-9][0-9]|200[0-2]', 
-                'iyr':'201[0-9]|2020', 
-                'eyr':'202[0-9]|2030', 
-                'hgt':'(1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6]|19[0-3])in', 
-                'hcl':'#[a-z0-9]{6}', 
-                'ecl':'amb|blu|brn|gry|grn|hzl|oth', 
-                'pid':'[0-9]{9}',
-                'cid':'.*'}
+fields_regex = {'byr':'^19[2-9][0-9]|200[0-2]$', 
+                'iyr':'^201[0-9]|2020$', 
+                'eyr':'^202[0-9]|2030$', 
+                'hgt':'^(1[5-8][0-9]|19[0-3])cm$|^(59|6[0-9]|7[0-6])in$', 
+                'hcl':'^#[a-f0-9]{6}$',
+                'ecl':'^amb$|^blu$|^brn$|^gry$|^grn$|^hzl$|^oth$', 
+                'pid':'^[0-9]{9}$',
+                'cid':'^.*$'}
 
 def parse_passport(passport):
     valid_passport = {'byr':False, 'iyr':False, 'eyr':False, 'hgt':False, 'hcl':False, 'ecl':False, 'pid':False}
