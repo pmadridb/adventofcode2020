e40bbc3a7d4f7d099f5ef882d392f79765bcb657
Pedro Madrid <pmadrid@gmail.com>
2020-12-09 09:17:39 -0400

    day 6 part 2

--- day6.py
+++ day6.py
@@ -14,4 +14,27 @@ def part1():
     unique_answers+= len(list(dict.fromkeys(answers)))
     print(unique_answers)
 
+def part2():
+    unique_answers = 0
+    answers = []
+    for line in answers_list:
+        if not line:
+            unique_answers+= dupe_answers(answers)
+            answers = []
+        else:
+            answers.append(line)
+    unique_answers+= dupe_answers(answers)
+    print(unique_answers)
+
+def dupe_answers(answers):
+    seen = {}
+    for answer in answers:
+        for char in answer:
+            if char not in seen:
+                seen[char] = 1
+            else:
+                seen[char] += 1
+    return list(seen.values()).count(len(answers))
+
 part1()
+part2()
